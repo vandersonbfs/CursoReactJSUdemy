2bac0627f28860107c37eb38ce8b2d7095b3bfd9
vandersonbfs <vanderson_b@hotmail.com>
2023-05-16 18:24:25 -0300

    Aula 9. Criando minhas funções

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ var nome = "Vanderson";
 // Escrever no html atraves do js
 // document.write("Aprendendo JavaScript");
 // Aula 07
+// Funcao entrar
+function entrar(){
+    var area = document.getElementById('area');
+    var texto = prompt('Digite seu nome: ');
+    if(texto == '' || texto == null){
+        alert('Digite um nome!');
+        area.innerHTML = 'Bem vindo ...';
+    }else{
+        area.innerHTML = 'Bem vindo ' + texto;
+    }
+   
+
+}
