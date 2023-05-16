4f7fcf59f5ae796397bd9c81c1b5aa7cb9d12dc7
vandersonbfs <vanderson_b@hotmail.com>
2023-05-16 17:42:34 -0300

    Criar botoes

---
+++ script.js
@@ -0,0 +1,7 @@
+// Aula 05
+var nome = "Vanderson";
+// Aula 06
+// var idade = prompt("Digite a sua idade:");
+// Escrever no html atraves do js
+// document.write("Aprendendo JavaScript");
+// Aula 07
